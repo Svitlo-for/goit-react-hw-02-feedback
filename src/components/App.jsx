9eb32fa5eec6fcfd9c8bddde1d4845c33950a475
Svitlo-for <serg_machigin@ukr.net>
2023-09-25 00:32:00 +0300

    add-feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  onLeaveFeedback = () => {
+    return;
+  };
+  countTotalFeedback = () => {
+    return this.setState.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return;
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={this.state}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
